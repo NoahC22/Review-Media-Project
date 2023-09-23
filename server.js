2f27b4c3b8ab7fe26a97510013c139381732bb21
Noah Castaneda <noahcast02@gmail.com>
2023-09-23 01:22:16 -0500

    Added routes and tested every page, all connects and works, next part is setting up mango db for keeping information.

--- server.js
+++ server.js
@@ -8,8 +8,41 @@ app.use(express.static('static'));
 app.use(express.urlencoded({extended: false}));
 app.set('view engine', 'ejs');
 
+app.get('/', async (req, res) => {
+    res.redirect('/home')
+})
+
 app.get('/home', async (req,res) => {
     res.render('homepage')
 })
+
+app.get('/list', async (req, res) => {
+    res.render('list_of_reviews')
+})
+
+app.get('/login', async (req, res) => {
+    res.render('login')
+})
+
+app.get('/signup', async (req, res) => {
+    res.render('signup')
+})
+
+app.get('/user', async (req, res) => {
+    res.render('user')
+})
+
+app.get('/review', async (req, res) => {
+    res.render('review_page')
+})
+
+app.get('/add', async (req, res) => {
+    res.render('add_review')
+})
+
+app.all('*', async (req, res) => {
+    res.sendStatus(404);
+});
+
 
 app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
